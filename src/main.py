342baf6132ff777482279f188b78efb57267eb33
iine4649 <shunsuke20070602honjo@gmail.com>
2025-09-23 09:56:45 -0400

    admin stuff

--- src/main.py
+++ src/main.py
@@ -7,11 +7,8 @@ from typing import List, Optional, Tuple
 from PIL import Image
 from PySide6.QtWidgets import QApplication
 
+from utils import ensure_data_dirs_exist, load_metadata, save_metadata, pixel_distance
 
-DATA_DIR = os.path.join("data")
-IMAGES_DIR = os.path.join(DATA_DIR, "images")
-METADATA_PATH = os.path.join(DATA_DIR, "metadata.json")
-NMH_MAP_PATH = os.path.join("assets", "nmh_map.png")
 
 
 @dataclass
